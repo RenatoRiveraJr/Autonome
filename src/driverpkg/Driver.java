e459232234e08a6683f1d60d876d9929a7cf8e6f
RenatoRiveraJr <renato.aspuria.rivera@gmail.com>
2017-05-06 11:05:00 -0700

    HouseKeeping
    + File Name Changes
    + Updated Driver to showcase full functionality added in previous commit

--- src/driverpkg/Driver.java
+++ src/driverpkg/Driver.java
@@ -1,7 +1,8 @@
 package driverpkg;
 
 import adapter.BuildAuto;
-
+import java.util.LinkedHashMap;
+import java.util.Map;
 /**
  * Created by RJ Rivera on 1/14/17.
  */
@@ -9,14 +10,33 @@ public class Driver {
 
     public static void main(String args[]) {
 
+        //HashMap
+        Map<model.Automobile, Integer> autoMap = new LinkedHashMap<model.Automobile, Integer>();
+
+        //Build 1st Automobile
         BuildAuto autoBuilder = new BuildAuto();
-        autoBuilder.buildAuto("BadFile.txt"); //Replace with GoodFile.txt to see working file
-
+        autoBuilder.buildAuto("Car1.txt");
         autoBuilder.printAuto();
-        autoBuilder.updateOptionSetName("FordZTW", "Transmission", "Gear Box");
+        autoMap.put(autoBuilder.getAuto(), autoMap.size()+1);
 
-        autoBuilder.updateOptionPrice("FordZTW", "Brakes/Traction Control",
-                                                "Standard", 5);
+        //Build 2nd Automobile
+        autoBuilder.buildAuto("Car2.txt");
+        System.out.println("----------------------------------------------------");
+        System.out.println("Car # 2");
         autoBuilder.printAuto();
+        autoMap.put(autoBuilder.getAuto(), autoMap.size() + 1);
+
+        //User Chosen Options
+        autoBuilder.readChosenOptions("optionChoices.txt");
+        autoBuilder.printChosenOptions();
+
+        //Iterate through LinkedHashMap
+        for (model.Automobile car: autoMap.keySet()) {
+            System.out.println(car.getMake() + " " + car.getModel() + " @ index: " + autoMap.get(car));
+        }
     }
 }
+/*
+autoBuilder.updateOptionSetName("FordZTW", "Transmission", "Gear Box");
+autoBuilder.updateOptionPrice("FordZTW", "Brakes/Traction Control", "Standard", 5);
+*/
